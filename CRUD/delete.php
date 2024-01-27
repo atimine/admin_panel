02b1c167b12a7d0fa0f02f2537eb0b54251cec8d
KhojiAkbarraxim <xraximovx020304@gmail.com>
2024-01-27 06:11:12 +0500

    added fetchall jsonbek

--- CRUD/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "connect.php";
-$sql=$conn->prepare("SELECT *FROM `users`");
-$sql->execute();
-$result=$sql->fetchall();
-$jsonbek=json_encode($result);
-print_r($jsonbek);
--- CRUD/read.php
+++ CRUD/read.php
@@ -3,15 +3,22 @@ include "connect.php";
 // if(i.sset($_POST['finddata']))
 // {
     // $find_data = $_POST['finddata'];
-    $sth = $conn->prepare("SELECT *FROM `users` WHERE `name` = '$find_data'");
-    $sth->execute();
-    $results = $sth->fetchAll();
-    foreach($results as $result)
-    {
-        $name = $result['name'];
-        $surname = $result['surname'];
-        $email = $result['email'];
-        $age = $result['age'];
+    // $sth = $conn->prepare("SELECT *FROM `users` WHERE `name` = '$find_data'");
+    // $sth->execute();
+    // $results = $sth->fetchAll();
+    // foreach($results as $result)
+    // {
+    //     $name = $result['name'];
+    //     $surname = $result['surname'];
+    //     $email = $result['email'];
+    //     $age = $result['age'];
 
-    }
+    // }
 // }
+
+$sql=$conn->prepare("SELECT *FROM `users`");
+$sql->execute();
+$result=$sql->fetchall();
+$jsonbek=json_encode($result);
+echo "<script>var jsonbek = '$jsonbek';</script>";
+// print_r($jsonbek);
